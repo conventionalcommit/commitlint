21bb2fb9d5eb66b03039933b0af10423e14083c8
muthukrishnan24 <muthukrishnanofficial@gmail.com>
2021-09-26 09:18:29 +0530

    refactor(cmd): move lint utils to lint.go

--- cmd/util.go
+++ cmd/util.go
@@ -9,52 +9,7 @@ import (
 	"path/filepath"
 	"runtime/debug"
 	"strings"
-
-	"github.com/conventionalcommit/commitlint/config"
-	"github.com/conventionalcommit/commitlint/lint"
 )
-
-func runLint(confFilePath, fileInput string) (lintResult string, hasError bool, err error) {
-	linter, format, err := getLinter(confFilePath)
-	if err != nil {
-		return "", false, err
-	}
-
-	commitMsg, err := getCommitMsg(fileInput)
-	if err != nil {
-		return "", false, err
-	}
-
-	res, err := linter.Lint(commitMsg)
-	if err != nil {
-		return "", false, err
-	}
-
-	resStr, err := format.Format(res)
-	if err != nil {
-		return "", false, err
-	}
-	return resStr, res.HasErrors(), nil
-}
-
-func getLinter(confFilePath string) (*lint.Linter, lint.Formatter, error) {
-	conf, err := config.GetConfig(confFilePath)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	format, err := config.GetFormatter(conf)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	linter, err := config.GetLinter(conf)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return linter, format, nil
-}
 
 func setGitConf(hookDir string, isGlobal bool) error {
 	var args = []string{"config"}
@@ -66,28 +21,6 @@ func setGitConf(hookDir string, isGlobal bool) error {
 	cmd := exec.Command("git", args...)
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
-}
-
-func getCommitMsg(fileInput string) (string, error) {
-	commitMsg, err := readStdInPipe()
-	if err != nil {
-		return "", err
-	}
-
-	if commitMsg != "" {
-		return commitMsg, nil
-	}
-
-	// TODO: check if currentDir is inside git repo?
-	if fileInput == "" {
-		fileInput = "./.git/COMMIT_EDITMSG"
-	}
-
-	inBytes, err := os.ReadFile(fileInput)
-	if err != nil {
-		return "", err
-	}
-	return string(inBytes), nil
 }
 
 func readStdInPipe() (string, error) {
